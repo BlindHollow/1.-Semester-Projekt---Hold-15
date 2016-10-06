ee9a530c63149c2be51081e6d086e43449d28e63
Mikkel Bytoft <bytoft@live.dk>
2016-10-06 10:38:53 +0200

    Tilføjede n, s, e, w, ne, nw, se og sw
    Tilføjede n, s, e, w, ne, nw, se og sw som mulige retninger

--- World of Zuul/src/worldofzuul/Game.java
+++ World of Zuul/src/worldofzuul/Game.java
@@ -1,22 +1,18 @@
 package worldofzuul;
 
 // TODO: Write Documentation
-public class Game
-{
+public class Game {
+
     private Parser parser;
     private Room currentRoom;
 
-
-    public Game()
-    {
+    public Game() {
         createRooms();
         parser = new Parser();
     }
 
-
-    private void createRooms()
-    {
-        Room outside1, outside2, helipad, hospital, policestation, grocerystore, firestation, house1, house2, drugstore, pub, gasstation ;
+    private void createRooms() {
+        Room outside1, outside2, helipad, hospital, policestation, grocerystore, firestation, house1, house2, drugstore, pub, gasstation;
 
         outside1 = new Room("on westside of the mainstreet");
         outside2 = new Room("on the eastside of the mainstreet");
@@ -26,11 +22,11 @@ public class Game
         grocerystore = new Room("in the grocerystore");
         firestation = new Room("in the firestation");
         house1 = new Room("in the red house");
-        house2 = new Room("in the blue house");        
-        drugstore = new Room("in the drugstore");      
+        house2 = new Room("in the blue house");
+        drugstore = new Room("in the drugstore");
         pub = new Room("in the pub");
         gasstation = new Room("in the gasstation");
-        
+
         hospital.setExit("east", outside1);
 
         outside1.setExit("northwest", drugstore);
@@ -38,45 +34,42 @@ public class Game
         outside1.setExit("southwest", firestation);
         outside1.setExit("southeast", pub);
         outside1.setExit("west", hospital);
-        
+
         drugstore.setExit("south", outside1);
 
         house1.setExit("south", outside1);
         house1.setExit("east", house2);
-        
-        firestation.setExit("north", outside1);        
 
-        pub.setExit("north", outside1);        
+        firestation.setExit("north", outside1);
+
+        pub.setExit("north", outside1);
 
         house2.setExit("west", house1);
         house2.setExit("south", outside2);
-        
+
         grocerystore.setExit("south", outside2);
-                 
+
         outside2.setExit("northwest", house2);
         outside2.setExit("northeast", grocerystore);
         outside2.setExit("southwest", policestation);
         outside2.setExit("southeast", gasstation);
         outside2.setExit("east", helipad);
-              
+
         policestation.setExit("north", outside2);
-        
+
         gasstation.setExit("north", outside2);
-        
+
         helipad.setExit("west", outside2);
-        
+
         currentRoom = hospital;
     }
 
-    public void play()
-    {
+    public void play() {
         printWelcome();
-
 
         boolean finished = false;
 
-        while (! finished)
-        {
+        while (!finished) {
             Command command = parser.getCommand();
             finished = processCommand(command);
         }
@@ -84,8 +77,7 @@ public class Game
         System.out.println("Thank you for playing.  Good bye.");
     }
 
-    private void printWelcome()
-    {
+    private void printWelcome() {
         System.out.println();
         System.out.println("Welcome to the World of Zuul!");
         System.out.println("World of Zuul is a new, incredibly boring adventure game.");
@@ -94,37 +86,35 @@ public class Game
         System.out.println(currentRoom.getLongDescription());
     }
 
-    private boolean processCommand(Command command)
-    {
+    private boolean processCommand(Command command) {
         boolean wantToQuit = false;
 
         CommandWord commandWord = command.getCommandWord();
 
-        if(commandWord == CommandWord.UNKNOWN)
-        {
+        if (commandWord == CommandWord.UNKNOWN) {
             System.out.println("I don't know what you mean...");
             return false;
         }
 
-        if (null != commandWord)
-        switch (commandWord) {
-            case HELP:
-                printHelp();
-                break;
-            case GO:
-                goRoom(command);
-                break;
-            case QUIT:
-                wantToQuit = quit(command);
-                break;
-            default:
-                break;
+        if (null != commandWord) {
+            switch (commandWord) {
+                case HELP:
+                    printHelp();
+                    break;
+                case GO:
+                    goRoom(command);
+                    break;
+                case QUIT:
+                    wantToQuit = quit(command);
+                    break;
+                default:
+                    break;
+            }
         }
         return wantToQuit;
     }
 
-    private void printHelp()
-    {
+    private void printHelp() {
         System.out.println("You are lost. You are alone. You wander");
         System.out.println("around at the university.");
         System.out.println();
@@ -132,38 +122,58 @@ public class Game
         parser.showCommands();
     }
 
-    private void goRoom(Command command)
-    {
-        if(!command.hasSecondWord())
-        {
+    private void goRoom(Command command) {
+        if (!command.hasSecondWord()) {
             System.out.println("Go where?");
             return;
         }
-
         String direction = command.getSecondWord();
+        if (null != command.getCommandWord());
+        switch (command.getSecondWord()) {
+            case "ne":
+                direction = "northeast";
+                break;
+            case "nw":
+                direction = "northwest";
+                break;
+            case "se":
+                direction = "southeast";
+                break;
+            case "sw":
+                direction = "southwest";
+                break;
+            case "n":
+                direction = "north";
+                break;
+            case "s":
+                direction = "south";
+                break;
+            case "e":
+                direction = "east";
+                break;
+            case "w":
+                direction = "west";
+                break;
+
+            default:
+                break;
+        }
 
         Room nextRoom = currentRoom.getExit(direction);
 
-        if (nextRoom == null)
-        {
+        if (nextRoom == null) {
             System.out.println("There is no door!");
-        }
-        else
-        {
+        } else {
             currentRoom = nextRoom;
             System.out.println(currentRoom.getLongDescription());
         }
     }
 
-    private boolean quit(Command command)
-    {
-        if(command.hasSecondWord())
-        {
+    private boolean quit(Command command) {
+        if (command.hasSecondWord()) {
             System.out.println("Quit what?");
             return false;
-        }
-        else
-        {
+        } else {
             return true;
         }
     }
