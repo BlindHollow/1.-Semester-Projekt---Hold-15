21a4e02246999026a85b05fc51270687ffce81c7
Kent vejrup Madsen <Blindedhollow@gmail.com>
2016-10-06 11:27:51 +0200

    Commit

--- World of Zuul/src/worldofzuul/Objects.java
+++ World of Zuul/src/worldofzuul/Objects.java
@@ -1,20 +1,2 @@
 package worldofzuul;
 
-class Objects
-{
-  // Objects name
-  public string name;
-
-  public Objects( string Name )
-  {
-    this.name = Name;
-
-  }
-
-  public void Found()
-  {
-    String ActionString = "You found " + this.name;
-    System.out.in.println( ActionString );
-  }
-
-}
