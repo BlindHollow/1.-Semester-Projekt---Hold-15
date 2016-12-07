96d912fcfa5ce0a0c63b2e88e4810fc8127ec3fa
Emil Johansen <emil@florint.dk>
2016-12-07 11:08:20 +0100

    LOADING NOW FULLY FUNCTIONAL BOYS, added loadGame, added load function to Application

--- World of Zuul/src/worldofzuul/Application.java
+++ World of Zuul/src/worldofzuul/Application.java
@@ -1,5 +1,9 @@
 package worldofzuul;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @author
@@ -9,15 +13,24 @@ class Application {
     private static Game detvirker;
 
     // Entry function, for the Java Program
-    public static void main(String[] Args) {
+    public static void main(String[] Args) throws IOException {
 
-        newGame();
+        Scanner keyboard = new Scanner(System.in);
+        String filename = keyboard.nextLine();
+        File file = new File(filename);
+        loadGame(file);
 
     }
 
     public static void newGame() {
         detvirker = new Game();
         detvirker.newGame(); //Creates a new game and starts it.
+    }
+
+    public static void loadGame(File file) throws IOException {
+        detvirker = new Game();
+        detvirker.loadGame(file);
+
     }
 
 } // Class Application
