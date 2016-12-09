bab414c9b402d42be15753b2601b3e482aece188
Kent vejrup Madsen <Blindedhollow@gmail.com>
2016-12-09 18:56:52 +0100

    they start with small lets now, unless they short names.

--- World of Zuul/src/worldofzuul/IO/FileFilters/Files.java
+++ World of Zuul/src/worldofzuul/IO/FileFilters/Files.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package worldofzuul.IO.FileFilters;
+package worldofzuul.IO.fileFilters;
 
 import java.io.*;
 
