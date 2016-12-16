7293539702bdc2e50a8fff71c20d0ada7e3e80ab
Kent vejrup Madsen <Blindedhollow@gmail.com>
2016-12-16 12:37:31 +0100

    comments added

--- World of Zuul/src/worldofzuul/Score/HighscorePlayer.java
+++ World of Zuul/src/worldofzuul/Score/HighscorePlayer.java
@@ -18,6 +18,7 @@ package worldofzuul.score;
 // Implements Comparable. used to sort a array
 public final class HighscorePlayer implements Comparable<HighscorePlayer>
 {
+    // Private Variables
     private String playerName = null;
     
     private boolean personOfInterest = false;
@@ -26,43 +27,78 @@ public final class HighscorePlayer implements Comparable<HighscorePlayer>
     
         // Accessors
             // Getters
+    /**
+     * Get the currentplayers Name
+     * @return 
+     */
     public String getPlayerName()
     {
         return this.playerName;
     }
     
+    /**
+     * Get the currentPlayers highscore
+     * @return 
+     */
     public int getPlayerScore()
     {
         return this.playerScore;
     }
     
+    /**
+     * Depends on the context, as an example if your looking for a player, in the list
+     * When it's sorted for you.
+     * You get if the currentObject is an interest to you
+     * @return 
+     */
     public boolean getPOI()
     {   
         return this.personOfInterest;
     }
     
         // Setters
+    /**
+     * Depends on the context, as an example if your looking for a player, in the list
+     * When it's sorted for you.
+     * You set if the currentObject is an interest to you
+     * @param PersonOfInterestState 
+     */
     public void setPOI( boolean PersonOfInterestState )
     {
         this.personOfInterest = PersonOfInterestState;
     }
     
+    /**
+     * 
+     * @param Score 
+     */
     public void setPlayerScore( int Score )
     {
         this.playerScore = Score;
     }
     
+    /**
+     * 
+     * @param name 
+     */
     public void setPlayerName( String name )
     {
         this.playerName = name;
     }
     
     // Init
+    /**
+     * Constructor
+     */
     public HighscorePlayer()
     {
         this.setPlayerScore(0);
     }
     
+    /**
+     * Constructor
+     * @param Name 
+     */
     public HighscorePlayer( String Name )
     {
         this();
@@ -70,6 +106,11 @@ public final class HighscorePlayer implements Comparable<HighscorePlayer>
         this.setPlayerName( Name );
     }
     
+    /**
+     * Constructor
+     * @param Name
+     * @param Score 
+     */
     public HighscorePlayer( String Name, int Score )
     {
         this( Name );
@@ -78,6 +119,9 @@ public final class HighscorePlayer implements Comparable<HighscorePlayer>
     }
     
     // Implementations
+    /**
+     * Used to sort a array, of characters depended on their highscore.
+     */
     @Override
     public int compareTo( HighscorePlayer object )
     {
